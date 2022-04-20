fa3ab0ee9764d8529fe92ae2db44913dfc46dba5
Bekzod <bekzodturgunoff@gmail.com>
2022-04-20 23:06:07 +0500

    learning..

--- src/hooks/UseMemo.jsx
+++ src/hooks/UseMemo.jsx
@@ -1,12 +1,8 @@
 import React, {useState, useEffect} from "react";
 
-
-
 export default function UseMemo() {
-
   const [data, setData] = useState()
-
-
+  const [toggle, setToggle] = useState(false)
 
   useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/comments")
@@ -24,12 +20,15 @@ export default function UseMemo() {
       }
     }
     console.log("THIS WAS COMPUTED")
-
     return longestName
   }
 
+  function make(){
+    setToggle(prev=>!prev)
+  }
 
-
-
-  return <div>{findLongestName(data)} </div>;
+  return <div>{findLongestName(data)} 
+  <button onClick={make}>Toggle</button>
+  {toggle && <h1>Toggle</h1>}
+  </div>;
 }
