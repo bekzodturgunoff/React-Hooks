18fd447835ccd3907fc48edd9c66482f5cafc294
bekzodturgunoff <bekzodturgunoff@gmail.com>
2022-04-20 11:09:57 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Reducer from "./hooks/Reducer";
 import Ref from "./hooks/Ref";
+import LayoutEffect from "./hooks/LayoutEffect";
 function App() {
   return (
     <div className="App">
       <Reducer />
       <Ref />
+      <LayoutEffect />
     </div>
   );
 }
--- src/hooks/LayoutEffect.jsx
+++ src/hooks/LayoutEffect.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useLayoutEffect, useEffect } from "react";
 
 export default function LayoutEffect() {
-  return (
-    <div>LayoutEffect</div>
-  )
+  useLayoutEffect(() => {
+    console.log("UseLayout");
+  }, []);
+
+  useEffect(() => {
+    console.log("UseEffect");
+  }, []);
+
+  return <div></div>;
 }
+
+// the only difference between two is that useLayoutEffect called before page loads
+// useLayoutEffect is used for pages and styling layouts
+// useEffect called after page loaded
